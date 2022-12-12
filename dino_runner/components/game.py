accf903a9c6c80aa7e641d7d4f25d5eda58f79fb
Victor Lieuthier <vgabriel@keener.io>
2022-12-12 10:47:46 -0300

    Create class dinosaur and the initials methods to run the game

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -9,6 +9,8 @@ from dino_runner.utils.constants import (
     TITLE,
 )
 
+from dino_runner.components.dinosaur import Dinosaur
+
 
 class Game:
     def __init__(self):
@@ -21,6 +23,7 @@ class Game:
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 300
+        self.player = Dinosaur()
 
     def run(self):
         # Game loop: events - update - draw
@@ -36,12 +39,15 @@ class Game:
                 self.playing = False
 
     def update(self):
+        user_input = pygame.key.get_pressed()
+        self.player.update(user_input)
         pass
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.player.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
